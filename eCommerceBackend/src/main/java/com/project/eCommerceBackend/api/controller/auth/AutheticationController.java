daea55473f45ab3f41c4e2d536ea4729166c87fb
Kamlesh <kamaleshmadan4@gmail.com>
2025-10-13 17:33:34 +0530

    added email verification Process

--- eCommerceBackend/src/main/java/com/project/eCommerceBackend/api/controller/auth/AutheticationController.java
+++ eCommerceBackend/src/main/java/com/project/eCommerceBackend/api/controller/auth/AutheticationController.java
@@ -4,7 +4,9 @@ package com.project.eCommerceBackend.api.controller.auth;
 import com.project.eCommerceBackend.api.model.LoginBody;
 import com.project.eCommerceBackend.api.model.LoginResponse;
 import com.project.eCommerceBackend.api.model.RegistrationBody;
+import com.project.eCommerceBackend.exception.EmailFailureException;
 import com.project.eCommerceBackend.exception.UserAlreadyExistException;
+import com.project.eCommerceBackend.exception.UserNotVerifiedException;
 import com.project.eCommerceBackend.model.LocalUser;
 import com.project.eCommerceBackend.service.UserService;
 import jakarta.validation.Valid;
@@ -28,19 +30,44 @@ public class AutheticationController {
             return ResponseEntity.ok().build();
         } catch (UserAlreadyExistException e) {
             return ResponseEntity.status(HttpStatus.CONFLICT).build();
-
+        } catch (EmailFailureException e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
         }
     }
 
     @PostMapping("/login")
     public ResponseEntity loginUser(@Valid @RequestBody LoginBody loginBody) {
-        String jwt = userService.loginUser(loginBody);
+        String jwt = null;
+        try {
+            jwt = userService.loginUser(loginBody);
+        } catch (UserNotVerifiedException ex) {
+            LoginResponse response = new LoginResponse();
+            response.setSuccess(false);
+            String reason = "USER_NOT_VERIFIED";
+            if (ex.isNewEmailSent()) {
+                reason += "_EMAIL_RESENT";
+            }
+            response.setFailureReason(reason);
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).body(response);
+        } catch (EmailFailureException ex) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
         if (jwt == null) {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
         } else {
             LoginResponse response = new LoginResponse();
             response.setJwt(jwt);
+            response.setSuccess(true);
             return ResponseEntity.ok(response);
+        }
+    }
+
+    @PostMapping("/verify")
+    public ResponseEntity verifyEmail(@RequestParam String token) {
+        if (userService.verifyUser(token)) {
+            return ResponseEntity.ok().build();
+        } else {
+            return ResponseEntity.status(HttpStatus.CONFLICT).build();
         }
     }
 
